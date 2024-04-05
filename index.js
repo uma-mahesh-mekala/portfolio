3b352d0fe7cf138fc09b6f3f36530233ac86751e
uma-mahesh-mekala <maheshuma154@gmail.com>
2024-04-05 08:58:15 +0530

    added animation to the hamburger menu

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.querySelector(".menu-bar").addEventListener("click", function () {
   document.querySelector(".nav-links").classList.toggle("active");
 });
+
+document.querySelector(".menu-bar").addEventListener("click", function () {
+  this.classList.toggle("cross");
+});
